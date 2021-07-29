c79aaa5381de1cc3f9aa0bdae64cbd6e598e0f82
RolandoCornejo <54423436+RolandoCornejo@users.noreply.github.com>
2021-07-29 09:48:56 -0600

    Primeros componentes

---
+++ pruebasreact/src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Todo from "./Todo";
+const Form = () => {
+  return (
+    <>
+      <h1>Form component</h1>
+      <Todo />
+    </>
+  );
+};
+export default Form;
